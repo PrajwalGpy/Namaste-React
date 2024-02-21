7a44a24e7a7954fb368e392c13243930cd1d25c3
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-02-21 16:37:33 +0530

    ep-1

---
+++ app.js
@@ -0,0 +1,15 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("button", { className: "btn" }, "push"),
+    React.createElement("button", { className: "btn" }, "popo"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("button", { className: "btn" }, "push"),
+    React.createElement("button", { className: "btn" }, "popo"),
+  ]),
+]);
+
+const hoo = React.createElement("h1", {}, "hee roopaly goog");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+root.render(element);
