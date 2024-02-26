65d3f42556abfa7c5a34703435df55faedb65f94
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-02-26 20:31:08 +0530

    created heafer of food delivering web app

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const JsxGoo = (
-  <h1 id="h1" className="goo">
-    Namste React 👨‍🍳
-  </h1>
-);
+/**
+ * header
+ * -->logo
+ * -->nav bar
+ * ------> home , about , cart
+ * body
+ * --> div
+ * -------> restorent
+ * footer
+ * -->contact,adddres,links,copyright
+ */
 
-const Name = () => <h1 className="goo">what is your name </h1>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://i.pinimg.com/originals/82/be/d4/82bed479344270067e3d2171379949b3.png"
+        ></img>
+      </div>
+      <div className="nav-iteams">
+        <ul>
+          <li>Home</li>
+          <li>about</li>
+          <li>contact</li>
+          <li>cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const Name1 = () => (
-  <div>
-    <h1 className="goo">what is your name prajwal gp </h1>
-    {JsxGoo}
-    <Name />
-  </div>
-);
-
+const AppLayout = () => {
+  return <div className="app"></div>;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Name1 />);
+root.render(<Header />);
