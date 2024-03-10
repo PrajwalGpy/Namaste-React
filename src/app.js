30c909776cb2d422281def6f71da2a81b2d7efd8
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-03-10 16:26:11 +0530

    ep - 5

--- src/app.js
+++ src/app.js
@@ -2,18 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./conponets/Header";
 
-/**
- * header
- * -->logo
- * -->nav bar
- * ------> home , about , cart
- * body
- * --> div
- * -------> restorent
- * footer
- * -->contact,adddres,links,copyright
- */
-
 const AppLayout = () => {
   return <div className="app"></div>;
 };
